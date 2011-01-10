17b760260c7af379e12369a25d82d7fb683b3891
mcarniel <mcarniel@1d0e9a7a-eec9-4ba9-a6b6-0d433d231dd9>
2011-01-10 20:01:06 +0000

    Release Notes 2.5.5 -------------------
    Added russian translations.
    Fixed bug on calculating sale item discounts.

---
+++ tags/2.5.5/srcmule/org/jallinone/variants/server/LoadProductVariantsMatrix.java
@@ -0,0 +1,74 @@
+package org.jallinone.variants.server;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import org.jallinone.commons.java.*;
+import javax.jws.WebService;
+
+import org.jallinone.items.java.DetailItemVO;
+import org.jallinone.items.java.GridItemVO;
+import org.jallinone.purchases.documents.java.SupplierPriceItemVO;
+import org.jallinone.purchases.pricelist.java.SupplierPriceVO;
+import org.jallinone.sales.documents.java.PriceItemVO;
+import org.jallinone.sales.pricelist.java.PriceVO;
+import org.jallinone.system.server.JAIOUserSessionParameters;
+import org.jallinone.variants.java.VariantsItemDescriptor;
+import org.jallinone.variants.java.VariantsMatrixVO;
+import org.openswing.swing.message.receive.java.Response;
+
+@WebService
+public interface LoadProductVariantsMatrix {
+
+	  /**
+	   * Business logic to execute.
+	   */
+	  public VariantsMatrixVO getDetailItemVariantsMatrix(
+	    DetailItemVO itemVO,
+	    String serverLanguageId,String username
+	  ) throws Throwable;
+
+	  
+	  /**
+	   * Business logic to execute.
+	   */
+	  public VariantsMatrixVO getGridItemVariantsMatrix(
+	    GridItemVO itemVO,
+	    String serverLanguageId,String username
+	  ) throws Throwable;
+	  
+	  
+	  /**
+	   * Business logic to execute.
+	   */
+	  public VariantsMatrixVO getSupplierPriceItemVariantsMatrix(
+	    SupplierPriceItemVO itemVO,
+	    String serverLanguageId,String username
+	  ) throws Throwable;
+	  
+	  
+	  /**
+	   * Business logic to execute.
+	   */
+	  public VariantsMatrixVO getSupplierPriceVariantsMatrix(
+	    SupplierPriceVO itemVO,
+	    String serverLanguageId,String username
+	  ) throws Throwable;
+	  	  
+	  
+	  /**
+	   * Business logic to execute.
+	   */
+	  public VariantsMatrixVO getPriceVariantsMatrix(
+	    PriceVO itemVO,
+	    String serverLanguageId,String username
+	  ) throws Throwable;
+	  
+	  
+	  /**
+	   * Business logic to execute.
+	   */
+	  public VariantsMatrixVO getPriceItemVariantsMatrix(
+	    PriceItemVO itemVO,
+	    String serverLanguageId,String username
+	  ) throws Throwable;
+	
+}
