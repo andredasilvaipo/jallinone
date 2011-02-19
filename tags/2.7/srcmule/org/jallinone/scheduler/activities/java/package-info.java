19f391979ed97fb0ceebca207066fbe5c83b1d74
mcarniel <mcarniel@1d0e9a7a-eec9-4ba9-a6b6-0d433d231dd9>
2011-02-19 10:10:35 +0000

    Release Notes 2.7 -----------------
    Refactored database tables related to hierarchies and translations, in order to fully support data decoupling among companies.
    Changed reports according to database changes.
    Fixed problem on customizing repository path for reports.
    Fixed problem on document management window, whem defining date type properties.
    Added multiple printing of invoices.
    Updated croatian translations.
    Include 2.3.8 version of OpenSwing.

---
+++ tags/2.7/srcmule/org/jallinone/scheduler/activities/java/package-info.java
@@ -0,0 +1,7 @@
+@javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters ({
+	@javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter(value=org.jallinone.commons.java.JavaSqlDateAdapter.class,type=java.sql.Date.class), 
+	@javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter(value=org.jallinone.commons.java.JavaSqlTimestampAdapter.class,type=java.sql.Timestamp.class),
+	@javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter(value=org.jallinone.commons.java.JavaAwtColorAdapter.class,type=java.awt.Color.class)
+})
+
+package org.jallinone.scheduler.activities.java;
